2a9486b241e45bcf8967e724e64e28fb7776ee1a
Hagb <hagb@hagb.name>
2023-01-27 20:30:56 +0800

    Replace response body of GET /com/server.crt with our own certification (mitmproxy)

--- mitmproxy-addon.py
+++ mitmproxy-addon.py
@@ -1,10 +1,27 @@
-from urllib.parse import urlparse
 import logging
-from mitmproxy import http, ctx
+from urllib.parse import urlparse
+from mitmproxy.certs import Cert
+from mitmproxy.http import HTTPFlow
+from mitmproxy.tls import TlsData
 
-
-def request(flow: http.HTTPFlow) -> None:
+def request(flow: HTTPFlow) -> None:
     if urlparse(flow.request.url).path in ('/por/conf.csp', '/por/rclist.csp'):
         print(f"TWFID has been captured: {flow.request.cookies.get('TWFID')}")
         print("Interrupt the connection!")
         flow.kill()
+
+
+def response(flow: HTTPFlow) -> None:
+    if urlparse(flow.request.url).path == '/com/server.crt':
+        if not flow.client_conn.certificate_list:
+            logging.error("No certificate on client_conn?!")
+            return
+        flow.response.content = flow.client_conn.certificate_list[0].to_pem()
+
+def tls_established_client(data: TlsData) -> None:
+    if data.ssl_conn is None:
+        return
+    cert = data.ssl_conn.get_certificate()
+    if cert is None:
+        return
+    data.conn.certificate_list = [Cert(cert.to_cryptography())]
